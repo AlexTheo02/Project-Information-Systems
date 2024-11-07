4293d692a34d77d9ce60c82633ab38f215593303
Andreas Georgiou <sdi2100218@di.uoa.gr>
2024-11-07 15:54:51 +0200

    read function

--- src/main.cpp
+++ src/main.cpp
@@ -41,40 +41,48 @@ int main (int argc, char* argv[]) {
     // Parse command line arguments
     parseArgs(argc, argv);
 
-    // Instantiate a DirectedGraph object DG
-    DirectedGraph<vector<float>> DG(euclideanDistance<vector<float>>, vectorEmpty<float>);
+    vector<vector<float>> data;
 
-    // Read base vectors file
-    vector<vector<float>> vectors = read_vecs<float>("data/siftsmall/siftsmall_base.fvecs", 10000);
+    ReadBin("data/contest-queries-release-1m.bin", 104, data);
 
-    // Create map key: vector, value: id (for groundtruth)
-    map<vector<float>, int> v2id;
-    for (int i=0; i<vectors.size(); i++){
-        v2id[vectors[i]] = i;
-    }
+    c_log << data << "\n";
 
-    // Read query vectors file
-    vector<vector<float>> queries = read_vecs<float>("data/siftsmall/siftsmall_query.fvecs", 100);
+    
 
-    // Read groundtruth file
-    vector<vector<int>> groundtruth = read_vecs<int>("data/siftsmall/siftsmall_groundtruth.ivecs", 100);
+    // // Instantiate a DirectedGraph object DG
+    // DirectedGraph<vector<float>> DG(euclideanDistance<vector<float>>, vectorEmpty<float>);
 
-    // Populate the Graph
-    for (auto& v : vectors){
-        DG.createNode(v);
-    }
+    // // Read base vectors file
+    // vector<vector<float>> vectors = read_vecs<float>("data/siftsmall/siftsmall_base.fvecs", 10000);
 
-    cout << "Running main program with:" << endl;
-    cout << "L: " << L << endl;
-    cout << "R: " << R << endl;
-    cout << "a: " << a << endl;
-    cout << "k: " << k << endl;
-    cout << "Number of threads: " << N_THREADS << endl;
-    if (!SHOULD_OMIT) { cout << "Debug mode" << endl; }
+    // // Create map key: vector, value: id (for groundtruth)
+    // map<vector<float>, int> v2id;
+    // for (int i=0; i<vectors.size(); i++){
+    //     v2id[vectors[i]] = i;
+    // }
 
-    profileVamana(L,R,a,k,DG,vectors,queries,groundtruth,v2id); // creating the vamana index and querying the groundtruths
+    // // Read query vectors file
+    // vector<vector<float>> queries = read_vecs<float>("data/siftsmall/siftsmall_query.fvecs", 100);
 
-    DG.store("graph_instance.txt"); // store the vamana Index
+    // // Read groundtruth file
+    // vector<vector<int>> groundtruth = read_vecs<int>("data/siftsmall/siftsmall_groundtruth.ivecs", 100);
+
+    // // Populate the Graph
+    // for (auto& v : vectors){
+    //     DG.createNode(v);
+    // }
+
+    // cout << "Running main program with:" << endl;
+    // cout << "L: " << L << endl;
+    // cout << "R: " << R << endl;
+    // cout << "a: " << a << endl;
+    // cout << "k: " << k << endl;
+    // cout << "Number of threads: " << N_THREADS << endl;
+    // if (!SHOULD_OMIT) { cout << "Debug mode" << endl; }
+
+    // profileVamana(L,R,a,k,DG,vectors,queries,groundtruth,v2id); // creating the vamana index and querying the groundtruths
+
+    // DG.store("graph_instance.txt"); // store the vamana Index
 
     return 0;
 }
